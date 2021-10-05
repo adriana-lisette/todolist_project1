5576c4c1e08512e32750af3961713245decaaeeb
Adriana García <agglisette@gmail.com>
2021-10-05 01:18:21 -0500

    Create toDo list

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,96 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '//ToDo:'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ToDoItemModel {
+  String text;
+  int order;
+  bool checked;
 
-  void _incrementCounter() {
+  ToDoItemModel(this.text, {required this.order, this.checked: false});
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ToDoItemModel> items = [
+    ToDoItemModel('A', order: 0),
+    ToDoItemModel('B', order: 1),
+    ToDoItemModel('C', order: 2),
+  ];
+
+  updateList(e) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      List<ToDoItemModel> checkeds =
+          this.items.where((element) => element.checked).toList();
+      checkeds.sort((a, b) => a.order - b.order);
+
+      List<ToDoItemModel> uncheckeds =
+          this.items.where((element) => !element.checked).toList();
+      uncheckeds.sort((a, b) => a.order - b.order);
+
+      this.items.clear();
+      this.items.addAll([...uncheckeds, ...checkeds]);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    items.isNotEmpty;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: false,
         title: Text(widget.title),
+        toolbarHeight: 180,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          ...this.items.map((e) => ListTile(
+                leading: Checkbox(
+                  checkColor: Colors.transparent,
+                  activeColor: Colors.grey.shade400,
+                  onChanged: (e) {},
+                  value: e.checked,
+                ),
+                title: Opacity(
+                  opacity: e.checked ? 0.5 : 1,
+                  child: Text(
+                    e.text,
+                    style: TextStyle(
+                      color: e.checked ? Colors.grey.shade500 : Colors.black,
+                      decoration: e.checked ? TextDecoration.lineThrough : null,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                    onPressed: () {
+                      print('More icon');
+                    },
+                    icon: Icon(Icons.more_vert)),
+                onTap: () {
+                  e.checked = !e.checked;
+                  updateList(e);
+                },
+              ))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
